c5f2d4d04303e58ecd361b7c7047f1a867591a96
devillu843 <84556464+devillu843@users.noreply.github.com>
2023-05-29 20:00:19 +0800

    Update train_for_class.py

--- train_for_class.py
+++ train_for_class.py
@@ -54,9 +54,7 @@ parser.add_argument('--save_txt_file', default='./jilu.txt',
 args = parser.parse_args()
 
 
-# ------------------------------------------
-# 模型调整
-# ------------------------------------------
+
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(device)
 if args.model_name == 'resnet50':
@@ -74,40 +72,26 @@ elif args.model_name == 'MobileNetV3':
 # if args.load_pt:
 #     model.load_state_dict(torch.load(args.load_pt))
 loss_function = nn.CrossEntropyLoss()
-# loss_function = SoftDiceLoss() # 学不到内容
 optimizer = torch.optim.Adam(model.parameters(), args.learning_rate)
-# stepLR
-# scheduler = torch.optim.lr_scheduler.StepLR(optimizer=optimizer, step_size=10, gamma=0.1)
-# 指数衰减
-# scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer=optimizer, gamma=0.9)
-# 余弦退火
-# scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer=optimizer, T_max=20, eta_min=0.001)
 
 
-# ------------------------------------------
-# 存储调整
-# ------------------------------------------
+
 image_path = './dataset/test-41-different/{}'.format(args.part)
-# 数据结果存储路径
+
 write_home = './logs/test-41-different/{}'.format(args.part)
 write_name = '/{}_pd{}/'.format(args.model_name, args.padding_value)
 write_path = write_home + write_name
-# 权重文件存储路径
+
 save_home = './weights/test-41-different/{}'.format(args.part)
 save_name = '/{}_pd{}.pt'.format(args.model_name, args.padding_value)
 save_path = save_home + save_name
-# 分类文件
+
 json_class = args.json_file
 excel_name = 'excel/{}-{}_pd{}_41_all_.xlsx'.format(
     args.model_name, args.part, args.padding_value)
 torch.manual_seed(100)
 torch.cuda.manual_seed(100)
 torch.cuda.manual_seed_all(100)
-
-
-# ------------------------------------------
-# 一堆创建路径
-# ------------------------------------------
 
 if os.path.exists(write_home):
     pass
@@ -124,30 +108,18 @@ else:
     os.mkdir(save_home)
 
 
-# 根据需要可写在循环内部或外部，查看相应的数据变化
+
 now = time.localtime()
 nowt = time.strftime("%Y-%m-%d-%H_%M_%S", now)
 
-# os.makedirs(write_path+'train_val'+nowt)
 writer = SummaryWriter(log_dir=write_path+nowt)
-# 写入特征层，特征图大小
-# if torch.cuda.is_available():
-#     graph_inputs = torch.from_numpy(np.random.rand(
-#         1, 3, input_shape[0], input_shape[1])).type(torch.FloatTensor).cuda()
-# else:
-#     graph_inputs = torch.from_numpy(np.random.rand(
-#         1, 3, input_shape[0], input_shape[1])).type(torch.FloatTensor)
-# write.add_graph(model, (graph_inputs,))
-# 写入loss,loss值，每一个step记录一次
-# write.add_scalar('Train_loss', loss, (epoch*epoch_size+iteration))
-
 
 data_transform = {
     'train': transforms.Compose([
                                 padding_img(args.padding_value),
                                 transforms.Resize((224, 224)),
                                 # transforms.CenterCrop((224,224)),
-                                transforms.RandomHorizontalFlip(),  # 以给定的概率随机水平翻转
+                                transforms.RandomHorizontalFlip(),  
                                 Gaussian(0.5, 0.1, 0.2),
                                 bright_contrast_color_sharpness(
                                     p=0.5, bright=0.5),
@@ -176,29 +148,6 @@ data_transform = {
 }
 
 
-# image_path = './dataset/test-14'
-# train_dataset = datasets.ImageFolder(
-#     root=image_path + '/train', transform=data_transform['train'])
-# train_num = len(train_dataset)
-
-# # 获取类
-# class_list = train_dataset.class_to_idx
-# # print(class_list.items())
-# class_dict = dict((key, value) for value, key in class_list.items())
-# # 写如json文件
-# json_str = json.dumps(class_dict, indent=4)
-# with open(json_class, 'w') as json_file:
-#     json_file.write(json_str)
-
-
-# train_images_path, train_images_label, val_images_path, val_images_label, test_images_path, test_images_label = read_split_data(
-#     root=image_path, class_json=json_class)
-
-
-# 模型大小
-# summary(model, input_size=(3, 224, 224))
-
-# print(save_path)
 
 train_acc = []
 train_loss = []
@@ -238,7 +187,7 @@ if args.train:
     best_acc = 0.0
 
     for epoch in range(args.epochs):
-        model.train()  # 训练时dropout有效
+        model.train()  
         loss = 0.0
         acc = 0
         loop_train = tqdm(enumerate(train_loader), total=len(train_loader))
@@ -246,19 +195,15 @@ if args.train:
             optimizer.zero_grad()
             outputs = model(images.to(device))
             predict = torch.max(outputs, dim=1)[1]
-            # outputs = nn.functional.log_softmax(outputs)
-            # output = nn.functional.soft_margin_loss(outputs,labels.to(device))
             loss_each_step = loss_function(outputs, labels.to(device))
             loss_each_step.backward()
             optimizer.step()
-            # scheduler.step()
 
             loss += loss_each_step.item()
             acc += (predict == labels.to(device)).sum().item()
             loop_train.set_description(
                 f'Train Epoch [{epoch+1}/{args.epochs}]')
             loop_train.set_postfix(loss=loss)
-        # 写入loss,loss值，每一个epoch记录一次
         acc_train = acc / train_num
         loss /= len(train_loader)
         writer.add_scalar('Train_acc', acc_train, epoch)
@@ -290,7 +235,6 @@ if args.train:
                 torch.save(model.state_dict(), save_path)
                 print('save the model:%.4f' % best_acc)
 
-            # 写入loss,loss值，每一个epoch记录一次
             writer.add_scalar('Val_acc', acc_val, epoch)
             writer.add_scalar('Val_loss', loss_each_step, epoch)
             val_acc.append(acc_val)
@@ -366,7 +310,7 @@ if args.test:
             predict_y = torch.max(outputs, dim=1)[1]
 
             # -----------------------------------------------
-            # 保存k个数据，与训练本身无关，郑老师的要求,写入excel文件
+
             k = 3
             output = F.softmax(outputs)
             out, pred_num = output.topk(k=k)
@@ -393,10 +337,8 @@ if args.test:
                         id3.append(cow_name)
                         p3.append(probability)
             # -----------------------------------------------
-
             y_true.extend(predict_y.to("cpu").numpy())
             y_pred.extend(test_labels.to("cpu").numpy())
-
             confusion.update(predict_y.to("cpu").numpy().astype(
                 'int64'), test_labels.to("cpu").numpy().astype('int64'))
             acc += (predict_y == test_labels.to(device)).sum().item()  # 指认设备
@@ -405,24 +347,15 @@ if args.test:
 
         t2 = time.perf_counter() - t1
 
-        # print(classification_report(y_true, y_pred, target_names=labels, digits=4))
+
         print('test time:', t2)
         print('pre test time:', (t2 / test_num) * 1000)
-        # print(y_true, y_pred)
         print('hamming;', metrics.hamming_loss(y_true, y_pred))
-        # print('jaccard:', metrics.jaccrd_similarity_score(y_true, y_pred))
         print('kappa:', metrics.cohen_kappa_score(y_true, y_pred))
 
-    # confusion.plot()
-    # confusion.summary()
+
     print("accurate_test:", accurate_test)
-    #     output = torch.squeeze(model(img))
-    #     predict = torch.softmax(output, dim=0)
-    #     predict_cla = torch.argmax(predict).numpy()
-    # print(class_indict[str(predict_cla)], predict[predict_cla].item())
-    # plt.show()
 
 f = args.save_txt_file
 with open(f, "a") as file:
     file.write(str(accurate_test)+"\n")
-a = 1
